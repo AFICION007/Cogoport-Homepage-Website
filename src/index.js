ce8231b1587289927e425ec43a8a13ec144797b5
Abhinav Tripathi <avinav0110@gmail.com>
2023-07-14 15:38:53 +0530

    adding our products

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
+import OurProducts from "./components/OurProducts";
 import TrustedBy from "./components/TrustedBy";
 import NextLevel from "./components/NextLevel";
 import HowItWorks from "./components/HowItWorks";
@@ -11,6 +12,7 @@ import IndustriesServed from "./components/IndustriesServed";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
+    <OurProducts/>
     <TrustedBy />
     <NextLevel />
     <HowItWorks />
